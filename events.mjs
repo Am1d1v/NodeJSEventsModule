e838a93e55aaf1a69fef5f5137510757379a027d
Am1d1v <dimadiman1adima@gmail.com>
2024-01-16 16:05:46 +0300

    Single event

--- events.mjs
+++ events.mjs
@@ -3,10 +3,24 @@ import {EventEmitter} from 'events';
 const myEmitter = new EventEmitter();
 
 // Add new Listeners
-myEmitter.on('timeout', () => {
-    console.log('Timeout Event');
+myEmitter.on('timeout', (secondsQuantity) => {
+    console.log(`Timeout event in ${secondsQuantity} seconds`);
 });
 
+// emitter timeouts
 setTimeout(() => {
-    myEmitter.emit('timeout');
+    myEmitter.emit('timeout', 1);
+}, 1000);
+
+setTimeout(() => {
+    myEmitter.emit('timeout', 2);
 }, 2000);
+
+// emit once
+myEmitter.once('singleEvent', () => {
+    console.log('Single Event');
+});
+
+setInterval(() => {
+    myEmitter.emit('singleEvent');
+},500);
