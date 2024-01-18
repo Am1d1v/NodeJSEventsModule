f24666fa62e075706a770058ac4c6b4c9ec83285
Am1d1v <dimadiman1adima@gmail.com>
2024-01-18 16:29:57 +0300

    fs events

--- FS-Events.mjs
+++ FS-Events.mjs
@@ -1,3 +1,24 @@
 import {EventListener} from 'events';
+import fs from 'fs';
 
 const fileEmitter = new EventListener();
+
+// Events Listeners
+// Write text data in file
+fileEmitter.on('writeComplete', () => {
+    console.log('Text File was written');
+
+    // Add new text Data
+    fs.appendFile('./text.txt', '\n New Data', () => {
+        fileEmitter.emit('appendComplete')
+    });
+});
+
+fileEmitter.on('appendComplete', () => {
+    console.log('New Data was added');
+})
+
+
+fs.writeFile('./text.txt', 'Some Data Text', () => {
+    fileEmitter.emit('writeComplite')
+});
