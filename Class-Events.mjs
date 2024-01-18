3e0f2004364a668517e53a00a81244fe8b980fbb
Am1d1v <dimadiman1adima@gmail.com>
2024-01-18 23:31:21 +0300

    NodeJS Class Events

--- Class-Events.mjs
+++ Class-Events.mjs
@@ -5,12 +5,25 @@ class Post extends EventEmitter {
         super();
         this.author = author;
         this.text = text;
-        this.likesQuantity
+        this.likesQuantity = 0;
     }
 
+    // Increase likesQuantity by 1;
     like(){
         this.likesQuantity += 1;
+        this.emit('likedPost');
     }
 }
 
 const myPost = new Post('AuthorName', 'PostText');
+
+myPost.on('likedPost', () => {
+    console.log(`Someone liked your post. Total likes on this post: ${myPost.likesQuantity}`);
+});
+
+console.log(myPost.author);
+console.log(myPost.text);
+console.log(myPost.likesQuantity);
+
+myPost.like();
+console.log(myPost.likesQuantity);
